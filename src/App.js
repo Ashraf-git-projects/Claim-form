0f674628d857274512cea5f32ac19f64538c60a1
Ashraf <ashrafhussain2265@gmail.com>
2025-07-15 22:32:27 +0530

    Version 2

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ import Details from './components/Details';
 function App() {
   return (
     <div className="App">
-      <img alt="bg_image" src={bg_img} className='bg_img'/>
+      <img alt="bg_image" src={bg_img} className='bg_img'/>   
       <div className="comp_layout">
         <div className='lft_part'>
          <Contact />
          <Details />
         </div>
-        
         <ClaimForm />
       </div>
-        
     </div>
   );
 }
--- src/components/ClaimForm.jsx
+++ src/components/ClaimForm.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../styles/ClaimForm.css";
 import { FaCalendarAlt } from "react-icons/fa";
-import { FaAngleDown } from "react-icons/fa";
 
 export default function ClaimForm() {
   return (
     <div className="claim_form_container">
       <h2 className="form_title">Claim Form</h2>
       <form className="claim_form">
+        <div className="pt_1">
         <div className="form_row">
           <input type="text" placeholder="First Name *" />
           <input type="text" placeholder="Last Name *" />
@@ -19,51 +19,47 @@ export default function ClaimForm() {
         </div>
 
         <div className="form_row">
-          {/* <div className="custom_input">
-            <div className="icon_field"> */}
+          <div className="custom_input">
+            <div className="icon_field">
               <input type="text" placeholder="Date of birth*" />
               <FaCalendarAlt className="field_icon" />
-            {/* </div>
-          </div> */}
+            </div>
+          </div>
           <input type="text" placeholder="Job Title *" />
         </div>
 
         <div className="form_row">
-          {/* <div className="custom_input">
-            <div className="icon_field"> */}
+          <div className="custom_input">
+            <div className="icon_field">
               <input type="text" placeholder="Date of Diagnosis *" />
               <FaCalendarAlt className="field_icon" />
-            {/* </div>
-          </div> */}
+            </div>
+          </div>
 
-          {/* <div className="custom_input">
+          <div className="custom_input">
            
-            <div className="icon_field"> */}
+            <div className="icon_field">
               <select>
                 <option>Type of Diagnosis*</option>
                 <option>Type A</option>
                 <option>Type B</option>
               </select>
-              <FaAngleDown className="field_icon" />
-            {/* </div>
-          </div> */}
+            </div>
+          </div>
         </div>
 
-        <div className="form_row full_width">
+        <div className="text_area">
           <textarea placeholder="Tell us your story (optional)" />
         </div>
-
+           </div>
         <div className="checkbox_group">
           <label>
             <input type="checkbox" />
-            I agree to the <span className="link">privacy policy</span> and{" "}
-            <span className="link">disclaimer</span> and give my express written
-            consent...
+            I agree to the privacy policy and disclaimer and give my express written consent to be contacted regarding my case options. I understand that I may be contacted using automatic dialing equipment. Message and data rates may apply. My consent does not require purchase. This is Legal advertising.
           </label>
-
           <label>
             <input type="checkbox" />
-            Please check this box to verify you’re a person.
+            Please check this box to verify you're a person.
           </label>
         </div>
 
